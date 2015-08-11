2d508cedc13c251999f3421199693e5dc121bf0e
IceMichael <71624129@qq.com>
2015-08-11 15:01:45 +0800

    add proj

---
+++ WeiBoM/WeiBoM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiBoM
+//
+//  Created by Michael on 8/10/15.
+//  Copyright (c) 2015 agIce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiBoM/WeiBoM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiBoM
+//
+//  Created by Michael on 8/10/15.
+//  Copyright (c) 2015 agIce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
